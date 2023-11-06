494d482d1371b0075656f5c8a6060540d3d2e253
Sayanc2000 <sayanc20002@gmail.com>
2023-11-06 15:50:38 +0530

    feat: functionality with middleware

---
+++ cacheMiddleware.py
@@ -0,0 +1,47 @@
+from typing import List
+
+from fastapi import Request
+from starlette.concurrency import iterate_in_threadpool
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import StreamingResponse, Response
+
+from caching.wrappers import retrieve_cache, create_cache
+
+
+class CacheMiddleware(BaseHTTPMiddleware):
+    def __init__(
+            self,
+            app,
+            cached_endpoints: List[str],
+    ):
+        super().__init__(app)
+        self.cached_endpoints = cached_endpoints
+
+    async def dispatch(self, request: Request, call_next) -> Response:
+        path_url = request.url.path
+        cache_control = request.headers.get('Cache-Control', None)
+        auth = request.headers.get('Authorization', "token public")
+        token = auth.split(" ")[1]
+
+        key = f"{path_url}_{token}"
+
+        if path_url not in self.cached_endpoints:
+            return await call_next(request)
+
+        stored_cache = await retrieve_cache(key)
+
+        res = stored_cache and cache_control != 'no-cache'
+
+        if not res:
+            response: Response = await call_next(request)
+            response_body = [chunk async for chunk in response.body_iterator]
+            response.body_iterator = iterate_in_threadpool(iter(response_body))
+
+            if response.status_code == 200:
+                await create_cache(response_body[0].decode(), key)
+            return response
+
+        else:
+            # If the response is cached, return it directly
+            json_data_str = stored_cache.decode('utf-8')
+            return StreamingResponse(iter([json_data_str]), media_type="application/json")
--- main.py
+++ main.py
@@ -1,46 +1,14 @@
 import time
-from starlette.concurrency import iterate_in_threadpool
+from fastapi import FastAPI
 
-from fastapi import FastAPI, Request
-from starlette.responses import StreamingResponse
-
-from caching.wrappers import retrieve_cache, create_cache
-
-app = FastAPI()
+from cacheMiddleware import CacheMiddleware
 
 cached_endpoints = [
     "/test"
 ]
 
-
-@app.middleware("http")
-async def cache_check_header(request: Request, call_next):
-    path_url = request.url.path
-    cache_control = request.headers.get('Cache-Control', None)
-    auth = request.headers.get('Authorization', "token public")
-    token = auth.split(" ")[1]
-
-    key = f"{path_url}_{token}"
-
-    if path_url not in cached_endpoints:
-        return await call_next(request)
-
-    stored_cache = await retrieve_cache(key)
-
-    res = stored_cache and cache_control != 'no-cache'
-
-    if not res:
-        response: StreamingResponse = await call_next(request)
-        response_body = [chunk async for chunk in response.body_iterator]
-        response.body_iterator = iterate_in_threadpool(iter(response_body))
-        await create_cache(response_body[0].decode(), key)
-
-        return response
-
-    else:
-        # If the response is cached, return it directly
-        json_data_str = stored_cache.decode('utf-8')
-        return StreamingResponse(iter([json_data_str]), media_type="application/json")
+app = FastAPI()
+app.add_middleware(CacheMiddleware, cached_endpoints=cached_endpoints)
 
 
 @app.get("/test")
